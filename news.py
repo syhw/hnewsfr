bc374fcf232d8aaa8fbed5e5582181766d34fb44
Gabriel Synnaeve <gabriel.synnaeve@gmail.com>
2009-03-04 18:03:40 +0900

    plus de texte ups

--- news.py
+++ news.py
@@ -4,25 +4,34 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
+from google.appengine.ext.db import polymodel
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import login_required
 from datetime import timedelta, datetime
-import urllib2
+import urllib2, math
 #from google.appengine.api import memcache
 #http://code.google.com/appengine/docs/python/memcache/usingmemcache.html#Memcache
 
-# (p-1)/(t+2)^1.5     p votes   t in hours
-
-class Entry(db.Model): 
+class Entry(polymodel.PolyModel): 
     author = db.UserProperty()
     content = db.StringProperty(multiline=True)
     date = db.DateTimeProperty(auto_now_add=True)
     ups = db.IntegerProperty()
 
+entry = Entry()
+entry.put()
+
 class Post(Entry):
     url = db.StringProperty()
     site = db.StringProperty()
     rank = db.IntegerProperty()
+
+class Comment(Entry):
+    post = db.ReferenceProperty(Entry)
+
+class Up(db.Model):
+    entry = db.ReferenceProperty(Entry)
+    voter = db.UserProperty()
 
 def calc_hours(post):
     diff = datetime.utcnow() - post.date
@@ -43,8 +52,24 @@ def count_comments(key):
     c.filter('post =', Post.get(key))
     return c.count() 
 
+#def count_comments(post):
+#    c = Comment.all()
+#    c.filter('post =', post)
+#    return c.count()
+
+def update_rank(post):
+    #post.rank = (3*post.ups + count_comments(post)) / (4*calc_hours(post) + 1)
+    return int(math.ceil(post.ups / math.pow(calc_hours(post) + 1, 1.5)))
+
+def user_found(u, e):
+# return 0 is not found and 1 if found #
+    v = Up.all()
+    v.filter('entry =', Entry.get(e))
+    v.filter('voter =', u)
+    return v.count()
+
 class DisplayPost(Post):
-    def __init__(self, post):
+    def __init__(self, post, u):
         self.hours = calc_hours(post)
         self.minutes = calc_minutes(post)
         self.comments_number = count_comments(post.key())
@@ -54,17 +79,18 @@ class DisplayPost(Post):
         self.url = post.url
         self.site = post.site
         self.id = (post.key()).id
+        if (u is None):
+            self.varw = 0
+        else:
+            self.varw = 1 - user_found(u, post.key())
 
 class DisplayPosts(list):
-    def __init__(self, posts):
+    def __init__(self, posts, u):
         for post in posts:
-            self.append(DisplayPost(post))
-
-class Comment(Entry):
-    post = db.ReferenceProperty(Entry)
+            self.append(DisplayPost(post, u))
 
 class DisplayComment(Comment):
-    def __init__(self, com):
+    def __init__(self, com, u):
         self.hours = calc_hours(com)
         self.minutes = calc_minutes(com)
         # self.comments_number = TODO
@@ -72,14 +98,26 @@ class DisplayComment(Comment):
         self.content = com.content
         self.ups = com.ups
         self.id = (com.key()).id
+        if (u is None):
+            self.varw = 0
+        else:
+            self.varw = 1 - user_found(u, com.key())
 
 class DisplayComments(list):
-    def __init__(self, comments):
+    def __init__(self, comments, u):
         for c in comments:
-            self.append(DisplayComment(c))
+            self.append(DisplayComment(c, u))
 
 class MainPage(webapp.RequestHandler):
     def get(self):
+        if calc_minutes(entry) > 20:
+            entry.date = datetime.utcnow()
+            entry.put()
+            posts_query = Post.all()
+            posts = posts_query.fetch(80) # HARDCODED LIMIT
+            for post in posts:
+                post.rank = update_rank(post)
+                post.put()
         posts_query = Post.all().order('-rank')
         posts = posts_query.fetch(20)
         if users.get_current_user():
@@ -89,12 +127,11 @@ class MainPage(webapp.RequestHandler):
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
         template_values = {
-            'posts': DisplayPosts(posts),
+            'posts': DisplayPosts(posts, users.get_current_user()),
             'url': url,
             'url_linktext': url_linktext,
             'submit': '/submit',
             'submit_linktext': 'Submit',
-            'user': users.get_current_user()
             }
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
@@ -130,51 +167,8 @@ class Submit(webapp.RequestHandler):
         if self.request.get('content'):
             submit.content = self.request.get('content')
         else:
-            print 'Comment tu veux que je fasse un lien sur la chaine vide ?'
-            print 'Empty string !'
-            print ''
-            print '                                  ,;;;;;;,'
-            print '                                ,;;;`""`;;|'
-            print '                              ,;;;/  .```,;|'
-            print '                            ,;;;;/   |    ||_'
-            print '                           /;;;;;    |    / .|'
-            print '                         ,;;;;;;|     `.  |/_/'
-            print '                        /;;;;;;;|       |'
-            print '             _,.---._  /;;;;;;;;|        ;   _.---.,_'
-            print '           .;;/      `.;;;;;;;;;|         ;`      |;;,'
-            print '         .;;;/         `;;;;;;;;;.._    .`         |;;;.'
-            print '        /;;;;|          _;-"`       `"-;_          |;;;;|'
-            print '       |;;;;;|.---.   .`  __.-"```"-.__  `.   .---.|;;;;;|'
-            print '       |;;;;;|     `|/  .`/__|     /__|`.  |/`     |;;;;;|'
-            print '       |;;;;;|       |_/ //  ||   //  || |_|       |;;;;;|'
-            print '       |;;;;;|       |/ |/    || ||    || ||       |;;;;;|'
-            print '        |;;;;|    __ || _  .-.|| |/.-.  _ || __    |;;;;/'
-            print '         |jgs|   / _||/ = /_o_|   /_o_| = ||/_ |   |;;;/'
-            print '          |;;/   |`.-     `   `   `   `     -.`|   |;;/'
-            print '         _|;`    | |    _     _   _     _    | /    `;|_'
-            print '        / .|      ||_  ( `--`(     )`--` )  _//      /. |'
-            print '        |/_/       |_/|  /_   |   |   _|  ||_/       |_|/'
-            print '                      | /|||  |   /  //|| |'
-            print '                      |  | |`._`-`_.`/ |  |'
-            print '                      |  ;  `-.```.-`  ;  |'
-            print '                      |   |    ```    /   |'
-            print '    __                ;    `.-"""""-.`    ;                __'
-            print '   /| |_         __..--|     `-----`     /--..__         _/ /|'
-            print '   |_`/|```---```..;;;;.`.__,       ,__.`,;;;;..```---```/|`_/'
-            print '        `-.__``;;;;;;;;;;;,,`._   _.`,,;;;;;;;;;;;``__.-`'
-            print '             ````--; ;;;;;;;;..`"`..;;;;;;;; ;--````   _'
-            print '        .-.       /,;;;;;;;`;;;;;;;;;`;;;;;;;,|    _.-` `|'
-            print '      .`  /_     /,;;;;;;`/| ;;;;;;; ||`;;;;;;,|  `|     `-`|'
-            print '     /      )   /,;;;;;`,` | ;;;;;;; | `,`;;;;;,|   |   .`-./'
-            print '     ``-..-`   /,;;;;`,`   | ;;;;;;; |   `,`;;;;,|   `"`'
-            print '              | ;;;`,`     | ;;;;;;; |  ,  `, ;;;`|'
-            print '             _|__.-`  .-.  ; ;;;;;;; ;  |`-. `-.__/_'
-            print '            / .|     (   )  |`;;;;;`/   |   |    /. |'
-            print '            |/_/   (`     `) |`;;;`/    `-._|    |_|/'
-            print '                    `-/ |-`   `._.`         `'
-            print '                      """      /.`|'
-            print '                               ||_/'
-            print ''
+            print
+            print 'Comment tu veux que je fasse un lien sur la chaine vide ? Empty string !'
             return -1
 
         submit.site = submit.url.split('/')[2] # should be a regexp ? 
@@ -199,12 +193,14 @@ class ViewComment(webapp.RequestHandler):
                     'post': post,
                     'url': url,
                     'url_linktext': url_linktext,
-                    'comments': DisplayComments(comments)
+                    'comments': DisplayComments(comments, None),
+                    'view': 0
                     }
         else:
             template_values = {
                     'post': post,
-                    'comments': DisplayComments(comments)
+                    'comments': DisplayComments(comments, users.get_current_user()),
+                    'view': 1 - user_found(users.get_current_user(), post.key())
                     }
         path = os.path.join(os.path.dirname(__file__), 'comment.html')
         self.response.out.write(template.render(path, template_values))
@@ -217,20 +213,39 @@ class ViewComment(webapp.RequestHandler):
         submit.content = self.request.get('content')
         pid = self.request.get('pid')
         submit.post = Post.get_by_id(int(pid))
+        submit.ups = 0
         submit.put()
         self.redirect('/comment?pid=' + pid)
+
+class Vote(webapp.RequestHandler):
+    @login_required     # Only work for get()
+    def get(self):
+        e = Post.get_by_id(int(self.request.get('for')))
+        if (user_found(users.get_current_user(), e.key())):
+            return -1
+        v = Up()
+        v.voter = users.get_current_user()
+        v.entry = e
+        v.put()
+        e.ups = e.ups + 1
+        if (e.__class__.__name__ == "Post"):
+            e.rank = update_rank(e)
+        e.put()
 
 # Never finish any code or you'll get old ...
 class WrongSubmit(webapp.RequestHandler):
     def get(self):
+        print 
         print "ton URL n'est pas browsable par mon urllib2 !"
 
 class AlreadySubmitted(webapp.RequestHandler):
     def get(self):
+        print 
         print "ton URL est deja dans la DB !"
 
 application = webapp.WSGIApplication(
                                     [('/', MainPage),
+                                     ('/vote', Vote),
                                      ('/submit', Submit),
                                      ('/comment', ViewComment),
                                      ('/wrong_submit', WrongSubmit),
