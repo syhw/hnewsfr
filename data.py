ffab8a83ce9753de6fe674e64b1eaecbb26bd8b1
Gabriel Synnaeve <gabriel.synnaeve@gmail.com>
2009-03-06 00:55:43 +0900

    refactoring en data.py et news.py

---
+++ data.py
@@ -0,0 +1,152 @@
+import math
+from google.appengine.ext import db
+from datetime import timedelta, datetime
+from google.appengine.ext.db import polymodel
+
+#from google.appengine.api import memcache
+#http://code.google.com/appengine/docs/python/memcache/usingmemcache.html#Memcache
+
+class Entry(polymodel.PolyModel): 
+    author = db.UserProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
+    ups = db.IntegerProperty()
+
+posts_limit = 800 # Hardcoded limit
+comments_limit = 400 # Hardcoded limit
+front_page_number = 20 # Hardcoded limit
+# Hack moche pour simuler un cron
+test_cron = Entry.all()
+test_cron.filter('content = ', 'FC')
+if test_cron.count(): # is_saved()
+    cron = Entry.get(test_cron.fetch(1)[0].key())
+else:
+    cron = Entry()
+    cron.content = "FC"
+    cron.put()
+
+class Post(Entry):
+    url = db.StringProperty()
+    site = db.StringProperty()
+    rank = db.IntegerProperty()
+    def get_comments(self):
+        com = Comment.all()
+        com.filter('post =', self)
+        # TODO Reply => recursively descend
+        com.order('date')
+        return com.fetch(comments_limit)
+
+class Comment(Entry):
+    post = db.ReferenceProperty(Entry)
+
+class Up(db.Model):
+    entry = db.ReferenceProperty(Entry)
+    voter = db.UserProperty()
+
+def calc_hours(post):
+    diff = datetime.utcnow() - post.date
+    hours = diff.seconds/3600  # :)
+    if diff.days is not -1:
+        hours = hours + diff.days*24
+    return hours
+
+def calc_minutes(post):
+    diff = datetime.utcnow() - post.date
+    minutes = diff.seconds/60  # :]
+    if diff.days is not -1:
+        minutes = minutes + diff.days*24
+    return minutes
+
+def count_comments(key):
+    c = Comment.all()
+    c.filter('post =', Post.get(key))
+    return c.count() 
+
+#def count_comments(post):
+#    c = Comment.all()
+#    c.filter('post =', post)
+#    return c.count()
+
+def update_rank(post):
+    #post.rank = (3*post.ups + count_comments(post)) / (4*calc_hours(post) + 1)
+    return int(math.ceil(post.ups / math.pow(calc_hours(post) + 1, 1.5)))
+
+def update_ranks():
+    posts_query = Post.all()
+    posts = posts_query.fetch(posts_limit) 
+    for post in posts:
+        post.rank = update_rank(post)
+        post.put()
+    return 0
+
+def update_check():
+    if calc_minutes(cron) > 19:
+        cron.date = datetime.utcnow()
+        cron.put()
+        update_ranks()
+
+def user_found(u, e):
+# return 0 is not found and 1 if found #
+    v = Up.all()
+    v.filter('entry =', Entry.get(e))
+    v.filter('voter =', u)
+    return v.count()
+
+def already(url):
+    test = Post.all()
+    test.filter("url = ", url)
+    return test.fetch(1)
+
+class DisplayPost(Post):
+    def __init__(self, post, u):
+        self.hours = calc_hours(post)
+        self.minutes = calc_minutes(post)
+        self.comments_number = count_comments(post.key())
+        self.author = post.author
+        self.content = post.content
+        self.ups = post.ups
+        self.url = post.url
+        self.site = post.site
+        self.id = (post.key()).id
+        if (u is None):
+            self.varw = 0
+        else:
+            self.varw = 1 - user_found(u, post.key())
+
+class DisplayPosts(list):
+    def __init__(self, posts, u):
+        for post in posts:
+            self.append(DisplayPost(post, u))
+
+class DisplayComment(Comment):
+    def __init__(self, com, u):
+        self.hours = calc_hours(com)
+        self.minutes = calc_minutes(com)
+        # self.comments_number = TODO
+        self.author = com.author
+        self.content = com.content
+        self.ups = com.ups
+        self.id = (com.key()).id
+        if (u is None):
+            self.varw = 0
+        else:
+            self.varw = 1 - user_found(u, com.key())
+
+class DisplayComments(list):
+    def __init__(self, comments, u):
+        for c in comments:
+            self.append(DisplayComment(c, u))
+
+class FeedPost(Post):
+    def __init__(self, post):
+        self.content = post.content
+        self.ups = post.ups
+        self.url = post.url
+        self.site = post.site
+        self.fdate = post.date.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+class FeedPosts(list):
+    def __init__(self, posts):
+        for post in posts:
+            self.append(FeedPost(post))
+
--- news.py
+++ news.py
@@ -1,123 +1,13 @@
-import cgi, os
-
+import cgi, os, data, urllib2
 from google.appengine.api import users
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext import db
-from google.appengine.ext.db import polymodel
 from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp.util import login_required
-from datetime import timedelta, datetime
-import urllib2, math
-#from google.appengine.api import memcache
-#http://code.google.com/appengine/docs/python/memcache/usingmemcache.html#Memcache
-
-class Entry(polymodel.PolyModel): 
-    author = db.UserProperty()
-    content = db.StringProperty(multiline=True)
-    date = db.DateTimeProperty(auto_now_add=True)
-    ups = db.IntegerProperty()
-
-# Hack moche pour simuler un cron
-test_cron = Entry.all()
-test_cron.filter('content = ', 'FC')
-if test_cron.count(): # is_saved()
-    cron = Entry.get(test_cron.fetch(1)[0].key())
-else:
-    cron = Entry()
-    cron.content = "FC"
-    cron.put()
-
-class Post(Entry):
-    url = db.StringProperty()
-    site = db.StringProperty()
-    rank = db.IntegerProperty()
-
-class Comment(Entry):
-    post = db.ReferenceProperty(Entry)
-
-class Up(db.Model):
-    entry = db.ReferenceProperty(Entry)
-    voter = db.UserProperty()
-
-def calc_hours(post):
-    diff = datetime.utcnow() - post.date
-    hours = diff.seconds/3600  # :)
-    if diff.days is not -1:
-        hours = hours + diff.days*24
-    return hours
-
-def calc_minutes(post):
-    diff = datetime.utcnow() - post.date
-    minutes = diff.seconds/60  # :]
-    if diff.days is not -1:
-        minutes = minutes + diff.days*24
-    return minutes
-
-def count_comments(key):
-    c = Comment.all()
-    c.filter('post =', Post.get(key))
-    return c.count() 
-
-#def count_comments(post):
-#    c = Comment.all()
-#    c.filter('post =', post)
-#    return c.count()
-
-def update_rank(post):
-    #post.rank = (3*post.ups + count_comments(post)) / (4*calc_hours(post) + 1)
-    return int(math.ceil(post.ups / math.pow(calc_hours(post) + 1, 1.5)))
-
-def user_found(u, e):
-# return 0 is not found and 1 if found #
-    v = Up.all()
-    v.filter('entry =', Entry.get(e))
-    v.filter('voter =', u)
-    return v.count()
-
-class DisplayPost(Post):
-    def __init__(self, post, u):
-        self.hours = calc_hours(post)
-        self.minutes = calc_minutes(post)
-        self.comments_number = count_comments(post.key())
-        self.author = post.author
-        self.content = post.content
-        self.ups = post.ups
-        self.url = post.url
-        self.site = post.site
-        self.id = (post.key()).id
-        if (u is None):
-            self.varw = 0
-        else:
-            self.varw = 1 - user_found(u, post.key())
-
-class DisplayPosts(list):
-    def __init__(self, posts, u):
-        for post in posts:
-            self.append(DisplayPost(post, u))
-
-class DisplayComment(Comment):
-    def __init__(self, com, u):
-        self.hours = calc_hours(com)
-        self.minutes = calc_minutes(com)
-        # self.comments_number = TODO
-        self.author = com.author
-        self.content = com.content
-        self.ups = com.ups
-        self.id = (com.key()).id
-        if (u is None):
-            self.varw = 0
-        else:
-            self.varw = 1 - user_found(u, com.key())
-
-class DisplayComments(list):
-    def __init__(self, comments, u):
-        for c in comments:
-            self.append(DisplayComment(c, u))
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        posts_query = Post.all().order('-rank')
+        posts_query = data.Post.all().order('-rank')
         posts = posts_query.fetch(20)
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
@@ -126,7 +16,7 @@ class MainPage(webapp.RequestHandler):
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
         template_values = {
-            'posts': DisplayPosts(posts, users.get_current_user()),
+            'posts': data.DisplayPosts(posts, users.get_current_user()),
             'url': url,
             'url_linktext': url_linktext,
             'submit': '/submit',
@@ -144,17 +34,14 @@ class Submit(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
 
     def post(self):
-        submit = Post()
+        submit = data.Post()
         if users.get_current_user():
             submit.author = users.get_current_user()
         else:
             self.redirect(users.create_login_url(self.request.uri))
         submit.url = self.request.get('url')
         # Already submited ? 
-        test = Post.all()
-        test.filter("url = ", submit.url)
-        res = test.fetch(1)
-        if res:
+        if data.already(submit.url):
             self.redirect('/already_submitted')
             return -1
         # Browsable ?
@@ -163,109 +50,80 @@ class Submit(webapp.RequestHandler):
         except:
             self.redirect('/wrong_submit')
             return -1
+        # Non-empty content
         if self.request.get('content'):
             submit.content = self.request.get('content')
         else:
-            print
-            print 'Comment tu veux que je fasse un lien sur la chaine vide ? Empty string !'
+            self.redirect('/empty_content')
             return -1
-
         submit.site = submit.url.split('/')[2] # should be a regexp ? 
         submit.rank = 0
         submit.ups = 0
         submit.put()
-        posts_query = Post.all()
-        posts = posts_query.fetch(80) # HARDCODED LIMIT
-        for post in posts:
-            post.rank = update_rank(post)
-            post.put()
+        data.update_ranks()
         self.redirect('/')
         return 0
 
 class ViewComment(webapp.RequestHandler):
     def get(self):
-        com = Comment.all()
-        post = Post.get_by_id(int(self.request.get('pid')))
-        com.filter('post =', post)
-        # TODO Reply => recursively descend
-        com.order('date')
-        comments = com.fetch(5000) # hardcoded limit
+        post = data.Post.get_by_id(int(self.request.get('pid')))
         if users.get_current_user() is None:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
             template_values = {
-                    'post': DisplayPost(post, None),
+                    'post': data.DisplayPost(post, None),
                     'url': url,
                     'url_linktext': url_linktext,
-                    'comments': DisplayComments(comments, None),
+                    'comments': data.DisplayComments(post.get_comments(), None),
                     'view': 0
                     }
         else:
             u = users.get_current_user()
             template_values = {
-                    'post': DisplayPost(post, u),
-                    'comments': DisplayComments(comments, u),
-                    'view': 1 - user_found(users.get_current_user(), post.key())
+                    'post': data.DisplayPost(post, u),
+                    'comments': data.DisplayComments(post.get_comments(), u),
+                    'view': 1 - data.user_found(u, post.key())
                     }
         path = os.path.join(os.path.dirname(__file__), 'comment.html')
         self.response.out.write(template.render(path, template_values))
     def post(self):
-        submit = Comment()
+        submit = data.Comment()
         if users.get_current_user():
             submit.author = users.get_current_user()
         else:
             self.redirect(users.create_login_url(self.request.uri))
         submit.content = self.request.get('content')
         pid = self.request.get('pid')
-        submit.post = Post.get_by_id(int(pid))
+        submit.post = data.Post.get_by_id(int(pid))
         submit.ups = 0
         submit.put()
         self.redirect('/comment?pid=' + pid)
+        return 0
 
 class Vote(webapp.RequestHandler):
     @login_required     # Only work for get()
     def get(self):
-        e = Post.get_by_id(int(self.request.get('for')))
-        if (user_found(users.get_current_user(), e.key())):
+        e = data.Post.get_by_id(int(self.request.get('for')))
+        if (data.user_found(users.get_current_user(), e.key())):
             return -1
-        v = Up()
+        v = data.Up()
         v.voter = users.get_current_user()
         v.entry = e
         v.put()
         e.ups = e.ups + 1
-        if (e.__class__.__name__ == "Post"):
-            e.rank = update_rank(e)
         e.put()
-
-class FeedPost(Post):
-    def __init__(self, post):
-        self.content = post.content
-        self.ups = post.ups
-        self.url = post.url
-        self.site = post.site
-        self.fdate = post.date.strftime("%Y-%m-%dT%H:%M:%SZ")
-
-class FeedPosts(list):
-    def __init__(self, posts):
-        for post in posts:
-            self.append(FeedPost(post))
+        if (e.__class__.__name__ == "Post"):
+            data.update_ranks()
+        else:
+            data.update_comments()
 
 class RSS(webapp.RequestHandler):
     def get(self):
-        ####### RE-RANK
-        if calc_minutes(cron) > 19:
-            cron.date = datetime.utcnow()
-            cron.put()
-            posts_query = Post.all()
-            posts = posts_query.fetch(400) # HARDCODED LIMIT
-            for post in posts:
-                post.rank = update_rank(post)
-                post.put()
-        ####### /RE-RANK
-        posts_query = Post.all().order('-rank')
+        data.update_check()
+        posts_query = data.Post.all().order('-rank')
         posts = posts_query.fetch(6)
         template_values = {
-            'posts': FeedPosts(posts),
+            'posts': data.FeedPosts(posts),
             }
         path = os.path.join(os.path.dirname(__file__), 'news.xml')
         self.response.headers["Content-Type"] = "application/rss+xml; charset=utf-8"
@@ -282,6 +140,11 @@ class AlreadySubmitted(webapp.RequestHandler):
         print 
         print "ton URL est deja dans la DB !"
 
+class EmptyContent(webapp.RequestHandler):
+    def get(self):
+        print
+        print 'Comment tu veux que je fasse un lien sur la chaine vide ? Empty string !'
+
 application = webapp.WSGIApplication(
                                     [('/', MainPage),
                                      ('/rss', RSS),
@@ -289,6 +152,7 @@ application = webapp.WSGIApplication(
                                      ('/submit', Submit),
                                      ('/comment', ViewComment),
                                      ('/wrong_submit', WrongSubmit),
+                                     ('/empty_content', EmptyContent),
                                      ('/already_submitted', AlreadySubmitted)],
                                     debug=True)
 
